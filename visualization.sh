27730f1f18f8ec80cacd6074f954d0ae4bcc35e2
Patricia Aguado Labrador <69902486+Patagualab@users.noreply.github.com>
2024-01-02 23:47:46 +0100

    Update visualization.sh

--- visualization.sh
+++ visualization.sh
@@ -4,7 +4,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$DIR/Flask_app"
 
+export FLASK_APP="controller.py"
+
+# En desarrollo
 flask run &
+
 sleep 2
 
 xdg-open http://127.0.0.1:5000
